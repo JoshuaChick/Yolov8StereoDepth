54097e6b48acb999990a61126cd321f8f0cd71d0
Joshua Chick <josh.sendemails@gmail.com>
2024-05-30 22:09:27 +1000

    Added placeholder for splitting camera feed

--- script.py
+++ script.py
@@ -2,7 +2,11 @@ import torch
 import cv2
 
 
-
+def split_camera_feed(img):
+    """
+    As the camera feed combines both cameras images into one, this function will split it and return the individual images.
+    """
+    pass
 
 
 
