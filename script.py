2c368de4ab4a709f5dc6a980f2b75ab9d8b034f5
Joshua Chick <josh.sendemails@gmail.com>
2024-05-30 22:17:58 +1000

    Added placeholder for determining depths function

--- script.py
+++ script.py
@@ -9,6 +9,15 @@ def split_camera_feed(img):
     pass
 
 
+def determine_depths(left_img, left_yolo_results, right_img, right_yolo_results):
+    """
+    Determines depths of every object in right_img. Returns a list of depths, the order of which corresponds to the
+    order of the right_yolo_results (e.g., if first depth is 1m and first object in results is person -> person is
+    1m away).
+    """
+    pass
+
+
 if __name__ == '__main__':
     pass
 
